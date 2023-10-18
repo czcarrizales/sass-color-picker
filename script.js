45794487863832aa265600794f8577f8d4aa6319
Chazz Carrizales <czcarrizales@yahoo.com>
2023-10-18 09:55:28 -0700

    use dominant, complementary, accent as color rules

--- script.js
+++ script.js
@@ -5,37 +5,37 @@ const getRandomColor = () => {
     let filteredColors = colors.filter(color => color !== selectedColor)
     const randomColor = filteredColors[Math.floor(Math.random() * filteredColors.length)]
     selectedColor = randomColor
-    document.querySelector('body').className = `bg-secondary-${randomColor}`
-    document.getElementById('main-container').className = `bg-primary-${randomColor} border-color-secondary-copy-${randomColor}`
-    document.querySelector('h1').className = `text-primary-copy-${randomColor}`
-    document.querySelector('h2').className = `text-primary-copy-${randomColor}`
+    document.querySelector('body').className = `bg-dominant-${randomColor}`
+    document.getElementById('main-container').className = `bg-complementary-${randomColor} border-color-dominant-copy-${randomColor}`
+    document.querySelector('h1').className = `text-complementary-copy-${randomColor}`
+    document.querySelector('h2').className = `text-complementary-copy-${randomColor}`
     document.querySelector('button').className = `bg-accent-${randomColor} text-accent-copy-${randomColor}`
     document.getElementById('current-mood').innerText = `${randomColor}`
-    document.getElementById('current-mood').className = `text-primary-copy-${randomColor}`
-    document.getElementById('outer').className = `bg-secondary-${randomColor} `
-    document.getElementById('middle').className = `bg-primary-${randomColor} `
+    document.getElementById('current-mood').className = `text-complementary-copy-${randomColor}`
+    document.getElementById('outer').className = `bg-dominant-${randomColor} `
+    document.getElementById('middle').className = `bg-complementary-${randomColor} `
     document.getElementById('inner').className = `bg-accent-${randomColor} `
-    document.getElementById('box1').className = `bg-primary-${randomColor} text-primary-copy-${randomColor} border-color-primary-copy-${randomColor}`
-    document.getElementById('box2').className = `bg-secondary-${randomColor} text-secondary-copy-${randomColor} border-color-primary-copy-${randomColor}`
-    document.getElementById('box3').className = `bg-accent-${randomColor} text-accent-copy-${randomColor} border-color-primary-copy-${randomColor}`
+    document.getElementById('box1').className = `bg-dominant-${randomColor} text-dominant-copy-${randomColor} border-color-dominant-copy-${randomColor}`
+    document.getElementById('box2').className = `bg-complementary-${randomColor} text-complementary-copy-${randomColor} border-color-complementary-copy-${randomColor}`
+    document.getElementById('box3').className = `bg-accent-${randomColor} text-accent-copy-${randomColor} border-color-complementary-copy-${randomColor}`
 
     //Shop Card
-    document.getElementById('card').className = `bg-primary-${randomColor} text-primary-copy-${randomColor} border-color-secondary-copy-${randomColor}`
+    document.getElementById('card').className = `bg-complementary-${randomColor} text-complementary-copy-${randomColor} border-color-dominant-copy-${randomColor}`
     document.getElementById('shop-card-button').className = `bg-accent-${randomColor} text-accent-copy-${randomColor} border-color-accent-copy-${randomColor}`
 
     //Profile Card
-    document.getElementById('profile-card').className = `bg-primary-${randomColor} border-color-secondary-copy-${randomColor}`
-    document.getElementById('profile-card-name').className = `text-primary-copy-${randomColor}`
-    document.getElementById('profile-card-email').className = `text-primary-copy-${randomColor}`
-    document.getElementById('profile-card-followers').className = `text-primary-copy-${randomColor}`
-    document.getElementById('profile-card-following').className = `text-primary-copy-${randomColor}`
+    document.getElementById('profile-card').className = `bg-complementary-${randomColor} border-color-dominant-copy-${randomColor}`
+    document.getElementById('profile-card-name').className = `text-complementary-copy-${randomColor}`
+    document.getElementById('profile-card-email').className = `text-complementary-copy-${randomColor}`
+    document.getElementById('profile-card-followers').className = `text-complementary-copy-${randomColor}`
+    document.getElementById('profile-card-following').className = `text-complementary-copy-${randomColor}`
     document.getElementById('profile-card-button').className = `bg-accent-${randomColor} text-accent-copy-${randomColor} border-color-accent-copy-${randomColor}`
 
     //Friends List
-    document.getElementById('friends-list').className = `bg-secondary-${randomColor} text-secondary-copy-${randomColor} border-color-secondary-copy-${randomColor}`
+    document.getElementById('friends-list').className = `bg-dominant-${randomColor} text-dominant-copy-${randomColor} border-color-dominant-copy-${randomColor}`
     const allFriendsListRows = document.querySelectorAll('.friends-list-row')
     for (let i = 0; i < allFriendsListRows.length; i++) {
-        allFriendsListRows[i].className = `friends-list-row bg-primary-${randomColor} text-primary-copy-${randomColor} border-color-primary-copy-${randomColor}`
+        allFriendsListRows[i].className = `friends-list-row bg-complementary-${randomColor} text-complementary-copy-${randomColor} border-color-complementary-copy-${randomColor}`
     }
     const allFriendsListRowButtons = document.querySelectorAll('.friends-list-row-button')
     for (let i = 0; i < allFriendsListRowButtons.length; i++) {
